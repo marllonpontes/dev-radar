7e7362c9b03c8ea7ce90fa97a62833cc81f368e7
Marllon Pontes <marllon_pontes@hotmail.com>
2020-04-04 15:55:32 -0300

    update back-end

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const http = require('http');
+
 const routes = require('./routes');
+const { setupWebsocket } = require('./websocket');
 
 const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-msjwx.mongodb.net/wweek10?retryWrites=true&w=majority', {
   useNewUrlParser: true,
@@ -27,4 +33,4 @@ app.use(routes);
 
  //MongoDB (Não-relacional)
 
-app.listen(3333);
+server.listen(3333);
