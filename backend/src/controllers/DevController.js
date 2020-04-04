865177a126de68d033197540153509f9791a9dc5
Marllon Pontes <marllon_pontes@hotmail.com>
2020-04-04 15:55:08 -0300

    update back-end

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const Dev = require('../models/Dev');
 const parseStringAsArray = require('../utils/parseStringAsArray');
+const { findConnections, sendMessage } = require('../websocket');
 
 // index, show, store, update, destroy
 
@@ -36,6 +37,15 @@ module.exports = {
         techs: techsArray,
         location,
       })
+
+      // Filtrar as conexões que estão no máximo 10km de distância
+      // e que o novo dev tenha pelo menos uma das tecnologias filtradas
+      const sendSocketMessageTo = findConnections(
+        { latitude, longitude },
+        techsArray,
+      )
+
+      sendMessage(sendSocketMessageTo, 'new-dev', dev);
 
     } else {
       return response.status(401).json({ error: 'User already exists.' });
